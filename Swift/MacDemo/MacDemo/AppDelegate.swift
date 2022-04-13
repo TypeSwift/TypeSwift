f78c0bfaf7064b14353ec073763b639eea2919cc
Justin Bush <revblaze@users.noreply.github.com>
2022-04-13 11:55:05 -0600

    Init mainWindow, 2-space indent

--- Swift/MacDemo/MacDemo/AppDelegate.swift
+++ Swift/MacDemo/MacDemo/AppDelegate.swift
@@ -9,22 +9,27 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
   
-
-
+  var mainWindow: NSWindow!
+  
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+    mainWindow = NSApplication.shared.windows[0]
+    NSApp.activate(ignoringOtherApps: true)
   }
-
+  
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
   }
-
+  
   func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
-
-
+  
+  // Handles Reopening of Main Window
+  func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag { for window in sender.windows { window.makeKeyAndOrderFront(self) }}
+    return true
+  }
+  
 }
 
--- Swift/MacDemo/MacDemo/ViewController.swift
+++ Swift/MacDemo/MacDemo/ViewController.swift
@@ -8,19 +8,19 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     // Do any additional setup after loading the view.
   }
-
+  
   override var representedObject: Any? {
     didSet {
-    // Update the view, if already loaded.
+      // Update the view, if already loaded.
     }
   }
-
-
+  
+  
 }
 
