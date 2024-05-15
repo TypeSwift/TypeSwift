56641597f3d6818ee1e375528a0ef480857b46f4
Justin Bush <jdevbush@gmail.com>
2024-05-15 06:36:13 -0600

    refactor: readable, modular

--- swiftgen/swiftgen.ts
+++ swiftgen/swiftgen.ts
@@ -4,21 +4,28 @@ import { convertType } from './typeMap';
 
 const exampleFiles = ["example.ts", "example1.ts", "example2.ts", "example3.ts", "example4.ts", "example5.ts"];
 
-exampleFiles.forEach(filePath => {
+function initializeProject(filePath: string) {
   const project = new Project();
-  const sourceFile = project.addSourceFileAtPath(filePath);
+  return project.addSourceFileAtPath(filePath);
+}
 
-  const variables = sourceFile.getVariableStatements().flatMap(statement => statement.getDeclarations().map(decl => decl.getName()));
-  const functions = sourceFile.getFunctions().map(func => ({
+function extractVariables(sourceFile: any) {
+  return sourceFile.getVariableStatements()
+    .flatMap((statement: any) => statement.getDeclarations().map((decl: any) => decl.getName()));
+}
+
+function extractFunctions(sourceFile: any) {
+  return sourceFile.getFunctions().map((func: any) => ({
     name: func.getName(),
-    parameters: func.getParameters().map(param => ({
+    parameters: func.getParameters().map((param: any) => ({
       name: param.getName(),
       type: convertType(param.getType().getText()),
       default: param.hasInitializer() ? param.getInitializer()?.getText() : undefined
     }))
   }));
+}
 
-  // Generate Swift code
+function generateSwiftCode(variables: string[], functions: any[]) {
   let swiftCode = `enum TypeSwift {\n\n  // Variables\n`;
   variables.forEach(variable => {
     swiftCode += `  case ${variable}\n`;
@@ -26,7 +33,7 @@ exampleFiles.forEach(filePath => {
 
   swiftCode += `\n  // Functions\n`;
   functions.forEach(func => {
-    const params = func.parameters.map(param => {
+    const params = func.parameters.map((param: any) => {
       const type = param.type === 'Optional' ? `${param.type}<${convertType('string')}>` : param.type;
       const defaultValue = param.default ? ` = ${param.default}` : '';
       return `_${param.name}: ${type}${defaultValue}`;
@@ -34,7 +41,24 @@ exampleFiles.forEach(filePath => {
     swiftCode += `  case ${func.name}(${params.replace(/_/g, '_ ')})\n`;
   });
 
-  fs.writeFileSync(`${filePath.replace('.ts', '')}_output.swift`, swiftCode);
+  swiftCode += `}\n`;
+  return swiftCode;
+}
 
+function writeSwiftCodeToFile(filePath: string, swiftCode: string) {
+  const outputFilePath = filePath.replace('.ts', '_output.swift');
+  fs.writeFileSync(outputFilePath, swiftCode);
   console.log(`Swift code generated successfully for ${filePath}!`);
+}
+
+exampleFiles.forEach(filePath => {
+  try {
+    const sourceFile = initializeProject(filePath);
+    const variables = extractVariables(sourceFile);
+    const functions = extractFunctions(sourceFile);
+    const swiftCode = generateSwiftCode(variables, functions);
+    writeSwiftCodeToFile(filePath, swiftCode);
+  } catch (error) {
+    console.error(`Error processing file ${filePath}:`, error);
+  }
 });
